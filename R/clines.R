e137779e0b275e11daef5555e3ec563c45857df7
Paul Murrell <paul@stat.auckland.ac.nz>
2004-03-01 00:00:00 +0000

    version 2.0-1

--- R/clines.R
+++ R/clines.R
@@ -1,8 +1,9 @@
 
 clines <- function(x, y, z, nlevels=10,
                    levels = pretty(range(z, na.rm=TRUE), nlevels)) {
+  warning("The clines package is no longer required.\nYou should use the function contourLines() in the graphics package instead.\n")
   invisible(.Call("clines", as.double(x), as.double(y),
-                  z, as.double(levels)))
+                  z, as.double(levels), PACKAGE="clines"))
 }
 
 
